40f5f63e1102f22ee5e3b03f041cf9ff32f3aa12
Kazeem David <davonkaze1@gmail.com>
2022-03-12 08:06:50 +0100

    final

--- app.js
+++ app.js
@@ -11,15 +11,28 @@ const indexerClient=new algosdk.Indexer(token, indexerServer, algod_port);
 const ASSET_ID=21364625;
 
 var voters=[];
+var mergedvoters=[];
+    
 var transactions=[];
 var signed=[];
 var committedAmount=0;
 var rewardAmount;
+var addresses=[{addr:'5CLFBFI33DFGZTCNTIG7FURBRC57E6JA2PI4PZC7PBGPQLXIFV4PCBVYHQ'
+},{addr:'6572Q2UUMYTVHKIZGZPQIHDQ3RIJ2EUE2IBIEHATZ7GUWSTZKU45PAM74E'}];
 
 var rewardMmemonic='horse name glow spider seek float thumb excess express below vanish furnace shallow charge cycle orchard ketchup laugh romance hope cushion this wrong absent scene'
 var secretKey=algosdk.mnemonicToSecretKey(rewardMmemonic); //Put address two Mnemonic Phrase here
 
-console.log(secretKey);
+function find(address){
+    var foundat;
+    for(var i=0; i<mergedvoters.length; i++){
+      if(mergedvoters[i].sender==address){
+        foundat=i;
+        break;
+      }
+    }
+    return foundat;
+}
 
 function truncateDecimals(number, digits) {
     var multiplier = Math.pow(10, digits),
@@ -30,28 +43,36 @@ function truncateDecimals(number, digits) {
 };
 
 
+
+
+
 async function getVoters(){
-    const txnHistory = await indexerClient
+       
+    for(address of addresses){
+        var txnHistory = await indexerClient
         .searchForTransactions()
-        .address('5CLFBFI33DFGZTCNTIG7FURBRC57E6JA2PI4PZC7PBGPQLXIFV4PCBVYHQ')
+        .address(address.addr)
         .assetID(ASSET_ID)
         .addressRole("receiver")
         .txType("axfer")
         .do();
 
-        await txnHistory.transactions.forEach((txn) => {
-            const transaction = txn["asset-transfer-transaction"];
-            committedAmount += transaction["amount"];
-          });
+       
 
-      const receiverAddress=await txnHistory.transactions.map(receiver=>{
+     await txnHistory.transactions.map(receiver=>{
           if((receiver['asset-transfer-transaction'].amount)/100 >1){
-            voters.push({
-                sender:receiver.sender,
-                amount:(receiver['asset-transfer-transaction'].amount)/100
-            })
+                voters.push({
+                    sender:receiver.sender,
+                    amount:(receiver['asset-transfer-transaction'].amount)/100
+                })
+            
+           
+            committedAmount+=(receiver['asset-transfer-transaction'].amount)/100;
           } 
         })
+    }
+        
+   
 }
 
 async function draftTransaction(voters, ratio){
@@ -59,25 +80,32 @@ async function draftTransaction(voters, ratio){
     voters.forEach((voter)=>{
             transactions.push(algosdk.makeAssetTransferTxnWithSuggestedParams(secretKey.addr, voter.sender, undefined, undefined,  (voter.amount*ratio)*100 , undefined, ASSET_ID, params));
     })
+    console.log(transactions)
 }
 
-getVoters().then(()=>{
-    if(committedAmount>=50000000){
-        rewardAmount=3000000;
-    }
-    else if(committedAmount>=70000000){
-        rewardAmount=6000000;
-    }
-    else if(committedAmount>=90000000){
-        rewardAmount=9000000
-    }
-    else{
-        reward=10;
-    }
-    var ratio=(rewardAmount*100)/committedAmount
-    ratio=truncateDecimals(ratio, 2);   
 
-    draftTransaction(voters, ratio).then(async()=>{
+getVoters().then(()=>{
+    mergedvoters.push(voters[0]);
+
+    for(var i=1; i<voters.length; i++){
+        var exists=find(voters[i].sender);
+        if(exists>=0){
+          mergedvoters[exists].amount+= voters[i].amount;
+        }
+        else{
+          mergedvoters.push(voters[i])
+        }
+    }
+    
+    rewardAmount=10;
+    console.log("committed: ", committedAmount)
+    var ratio=rewardAmount/committedAmount
+    ratio=truncateDecimals(ratio, 2);   
+    console.log("ratio: ",ratio)
+    console.log(mergedvoters)
+
+    draftTransaction(mergedvoters, 1).then(async()=>{
+      
         let txgroup = algosdk.assignGroupID(transactions);
         transactions.forEach((transaction)=>{
             var signedTransaction=transaction.signTxn(secretKey.sk );
@@ -85,6 +113,8 @@ getVoters().then(()=>{
         })
 
         let tx = await algodClient.sendRawTransaction(signed).do();
+      
+
         console.log("Transaction : " + tx.txId);
     })  
 });
